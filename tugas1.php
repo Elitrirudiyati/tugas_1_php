5cd4dca83d90a77c7498d9cdf2d5c367be003a39
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-10-07 13:20:37 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	
+	<?php
+	$menu1="Ayam Goreng";
+	$menu2="Ayam Bakar";
+	$menu3="Ayam Sayur";
+	$menu4="Tempe Goreng";
+	$menu5="Tahu Goreng";
+	$menu6="Soto";
+	$menu7="Nasi Putih";
+	$menu8="Es Teh";
+	$menu9="Es Tebu";
+
+	$harga1="= Rp. 13000";
+	$harga2="=Rp. 15000";
+	$harga3="=Rp. 13000";
+	$harga4="=Rp. 1000";
+	$harga5="=Rp. 1000";
+	$harga6="=Rp. 3000";
+	$harga7="=Rp. 5000";
+	$harga8="=Rp. 4000";
+	$harga9="=Rp. 5000";
+
+	
+
+
+	
+	echo "<h2>Daftar Makanan</h2>";
+	echo "<br";
+	echo "<ul>
+	<li>".$menu1.$harga1."</li>
+	<li>".$menu2.$harga2."</li>
+	<li>".$menu3.$harga3."</li>
+	<li>".$menu4.$harga4."</li>
+	<li>".$menu5.$harga5."</li>
+	<li>".$menu6.$harga6."</li>
+	<li>".$menu7.$harga7."</li>
+	<li>".$menu8.$harga8."</li>
+	<li>".$menu9.$harga9."</li>
+	</ul>";
+?>
+</body>
+</html>
